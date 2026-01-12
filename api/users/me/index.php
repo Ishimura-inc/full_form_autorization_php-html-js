144aa0d9890b6cab7be2d87b63adf02528dca14b
Zima <fenix6391@gmail.com>
2026-01-12 16:24:08 +0700

    Update index.php

--- api/users/me/index.php
+++ api/users/me/index.php
@@ -1,6 +1,13 @@
 <?php
 declare(strict_types=1);
 
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
+    exit;
+}
+
 require_once __DIR__ . '/../../../core/auth.php';
 require_once __DIR__ . '/../../../core/db.php';
 
