f6ab5b2a0090dbb206f1dd04962df5764811b37c
Zima <fenix6391@gmail.com>
2026-01-12 16:23:37 +0700

    Update check_auth.php

--- api/check_auth.php
+++ api/check_auth.php
@@ -1,5 +1,13 @@
 <?php
 declare(strict_types=1);
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
+    exit;
+}
+
 require_once __DIR__ . '/../core/auth.php';
 
 header('Content-Type: application/json; charset=utf-8');
